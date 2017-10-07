745b06c39161f5e23ecdbbfc1e8ed5c7d724a451
laercio Silva <lndsilva@gmail.com>
2017-10-07 11:06:50 -0300

    Inclusao da butternife

---
+++ app/src/main/java/br/com/fiap/pizzaria/model/pedido.java
@@ -0,0 +1,38 @@
+package br.com.fiap.pizzaria.model;
+
+import java.util.List;
+
+/**
+ * Created by logonrm on 07/10/2017.
+ */
+
+public class pedido {
+
+    private List<String> sabor;
+    private String tamanho;
+    private String tipoPagamento;
+
+    public List<String> getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(List<String> sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+}
